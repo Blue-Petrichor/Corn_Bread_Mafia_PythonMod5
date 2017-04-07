94d9e6d05809140a9a4c0c0dc8a6bb22c4ef3e73
Michael Shaw <Michaelshaw3@mail.weber.edu>
2017-04-07 09:25:31 -0600

    Adding wrapper.sh

---
+++ wrapper.sh
@@ -0,0 +1,26 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: wrapper.sh
+# 
+#         USAGE: ./wrapper.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Michael Shaw (), Michaelshaw3@mail.weber.edu
+#  ORGANIZATION: 
+#       CREATED: 04/07/2017 09:24
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+
+
+
+exit 0
+
