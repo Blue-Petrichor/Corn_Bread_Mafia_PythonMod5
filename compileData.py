b9769f8b023a42f7656a59c086594467f7985294
Blake Johnson <blakejohnson6@mail.weber.edu>
2017-04-12 17:56:37 -0600

    Made some changes to compileData query to lessen difficulty in building fixed string

--- compileData.py
+++ compileData.py
@@ -16,38 +16,44 @@ def query_db(begDate, endDate):
         no idea yet
     """
 
-
-
-
-    #SELECT trans.trans_id, trans_line.qty, products.prod_desc FROM trans JOIN trans_line, products WHERE trans.trans_id = trans_line.trans_id AND trans_line.prod_num = products.prod_num;
-
-
-    sqlString = 'SELECT trans.trans_id, trans.trans_date, trans.card_num, trans_line.qty, trans_line.amt, products.prod_desc, trans.total \
-    FROM trans JOIN trans_line, products \
-    WHERE trans.trans_id = trans_line.trans_id \
-    AND trans_line.prod_num = products.prod_num \
-    AND trans.trans_date >= DATETIME({0}) \
-    AND trans.trans_date <= DATETIME({1});'.format(begDate, endDate)
-
+    sqlTransID = 'SELECT trans_id, trans_date, card_num, total FROM trans \
+    WHERE trans_date >= DATETIME({0}) \
+    AND trans_date <= DATETIME({1});'.format(begDate, endDate)
 
     conn, cur = db.open_db('hw8SQLite.db')
-    recs = db.query_db(sqlString, cur)
+    recs = db.query_db(sqlTransID, cur)
 
-    for row in recs:
-        print(row)
+    for tranID in recs:
+        # Create empty list for fixed string info
+        fixed = []
+        fixed.append(tranID[0])
+        fixed.append(tranID[1])
+        fixed.append(tranID[2])
+
+        sqlProdInfo = 'SELECT trans_line.qty, trans_line.amt, products.prod_desc \
+            FROM trans_line \
+            JOIN products \
+            WHERE trans_line.prod_num = products.prod_num \
+            AND trans_id = {};'.format(tranID[0])
+
+        prod_recs = db.query_db(sqlProdInfo, cur)
+        for row in prod_recs:
+            fixed.append(row[0])
+            fixed.append(row[1])
+            fixed.append(row[2])
+        fixed.append(tranID[3])
+        print(fixed)
 
     db.close_db(conn, cur)
+
 
 def main():
     """
     testing
     """
-
-    #have to have quotes for query to work properly
-    query_db('\'2017-03-23\'', '\'2017-05-21\'')
-
+    # have to have quotes for query to work properly
+    query_db('\'2017-03-23 00:00 \'', '\'2017-05-21 23:59\'')
 
 if __name__ == "__main__":
     main()
     exit(0)
-
