1ccb3bd5a6a90e0b16a777d316a91676caac7b47
Blake Johnson <blakejohnson6@mail.weber.edu>
2017-04-15 18:51:17 -0600

    added exit handling. added ftp access.

--- create_report.py
+++ create_report.py
@@ -3,6 +3,7 @@ import sys
 from datetime import datetime
 import compileData
 import sendemail
+import ftpaccess
 
 
 def make_report(recs, begDate, endDate):
@@ -11,11 +12,13 @@ def make_report(recs, begDate, endDate):
     Args:
         recs -> list of things
     """
-    fp = open("company_trans_{0}_{1}.dat".format(begDate, endDate), 'w')
+    fileName = "company_trans_{0}_{1}.dat".format(begDate, endDate)
+    fp = open(fileName, 'w')
     for item in recs:
         fp.write(item)
         fp.write('\n')
     fp.close()
+    return fileName
 
 
 def convert_date(begDate, endDate):
@@ -52,6 +55,12 @@ def main():
         begDate, endDate = convert_date(sys.argv[1], sys.argv[2])
     except ValueError:
         print("Bad date format")
+        subStr = 'The create_report program exit with code 1'
+        sendemail.send_email(sys.argv[3],
+                             'email_templates/BadParam.txt',
+                             subStr,
+                             sys.argv[1],
+                             sys.argv[2])
         exit(1)
 
     # add ' to date strings for query to work
@@ -62,14 +71,25 @@ def main():
 
     if len(strs) == 0:
         print("No records found. Exiting...")
+        subStr = 'The create_report program exit with code -2'
+        sendemail.send_email(sys.argv[3],
+                             'email_templates/NoTrans.txt',
+                             subStr,
+                             sys.argv[1],
+                             sys.argv[2])
         exit(2)
 
     for x in strs:
         print(x)
         # print(len(x))
 
-    make_report(strs, sys.argv[1], sys.argv[2])
-    sendemail.send_email(sys.argv[3], 'email_templates/Success.txt', begDate,
+    fileName = make_report(strs, sys.argv[1], sys.argv[2])
+    ftpaccess.upload_to_ftp(fileName, sys.argv[4], sys.argv[5])
+    subStr = 'Successfully transfer file (FTP Address)'
+    sendemail.send_email(sys.argv[3],
+                         'email_templates/Success.txt',
+                         subStr,
+                         begDate,
                          endDate)
 
 if __name__ == "__main__":
